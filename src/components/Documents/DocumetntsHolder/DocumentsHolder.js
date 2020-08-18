376eadef708b8f4a783d81186b7810f7bf964340
TeemourA <taxprogramming@gmail.com>
2020-08-18 17:08:45 +0300

    add terms for enterpreneur & legal

--- src/components/Documents/DocumetntsHolder/DocumentsHolder.js
+++ src/components/Documents/DocumetntsHolder/DocumentsHolder.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './DocumentHolder.css';
-import Terms from '../TermsOfUse/TermsOfUse';
+import EnterpreneurTerms from '../TermsOfUse/TermsOfUseEnterpreneur';
+import LegalTerms from '../TermsOfUse/TermsOfUseLegal';
 import Return from '../TermsOfReturn/TermsOfReturn';
 
 const form = (props) => {
-    return (
-        <div className="Documents">
-            <Terms requisites={props.requisites}/>
-            <Return requisites={props.requisites}/>
-        </div>
-    );
+  return (
+    <div className="Documents">
+      {props.active === 'legal' ?
+        <LegalTerms requisites={props.requisites} /> :
+        <EnterpreneurTerms requisites={props.requisites} />}
+      <Return requisites={props.requisites} />
+    </div>
+  );
 };
 
 export default form;
