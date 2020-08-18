c12d3ab78e6e2c71e6fba0776da23758c047e891
truehusla <nirumiantsev@gmail.com>
2020-08-18 14:02:39 +0300

    Refactor file structure, add terms of return module

--- src/components/Documents/DocumetntsHolder/DocumentsHolder.js
+++ src/components/Documents/DocumetntsHolder/DocumentsHolder.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './DocumentHolder.css';
-import Terms from '../TermsOfUse';
+import Terms from '../TermsOfUse/TermsOfUse';
+import Return from '../TermsOfReturn/TermsOfReturn';
 
 const form = (props) => {
     return (
         <div className="Documents">
             <Terms requisites={props.requisites}/>
+            <Return requisites={props.requisites}/>
         </div>
     );
 };
